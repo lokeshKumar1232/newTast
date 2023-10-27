91e6d1cb217ad2186ab4d0d3acc74fbc7a8d2eac
lokesh <lokeshsonkh02@gmail.com>
2023-10-27 15:05:23 +0530

    code

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {View,Text,TouchableOpacity} from "react-native"
-import AppNavigator from "./src/Navigation/AppNavigator";
-const App =()=>{
-  return(
-   <AppNavigator/>
-  )
-}
-export default App
